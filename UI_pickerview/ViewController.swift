86adae931275b86faa9b06ff9890c6e23e5bb9de
COE-030 <coe-030@coe-030s-imac.local>
2019-09-04 13:52:36 +0530

    Initial Commit

---
+++ UI_pickerview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UI_pickerview
+//
+//  Created by COE-030 on 04/09/19.
+//  Copyright © 2019 COe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
